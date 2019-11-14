4a83d736dda11abddbfe4e7cb827d9a00f6bb3b0
rdieu <rdieuju1@fau.edu>
2019-11-14 13:55:10 -0500

    Updated

--- problems/08-shapes/08-shapes.js
+++ problems/08-shapes/08-shapes.js
@@ -20,6 +20,42 @@ Example for printShape("Diamond", 5, "*");
  ***
   * 
 */
-function printShape(shape, height, character) {
+
+function square(line){
+  let height = 5, edge = '%', shape = ' ';
   
-}
+  for (let i = 1; i <= height; i++) {
+      if (i === 1 || i === height) {
+          line = Array(height + 1).join(edge);
+      } else {
+          line = edge + Array(height - 1).join(shape) + edge;
+      }
+      console.log(line);
+  }
+  }
+  square(5);
+  
+  
+  function pyramid(n) { 
+  for (let i = 1; i <= n; i++) {
+    let s = "";
+    for (let j = 1; j <= (2 * n - 1); j++) { 
+       (j >= n + 1 - i && j <= n - 1 + i) ? s += "$" : s += " "; 
+     }
+    console.log(s);
+   }
+  }
+  pyramid(5);
+  
+  
+  
+  function diamond(v){
+  let y, w, shape = '';
+  
+  for(y = 0; y < v * 2 - 1; y++) {
+    w = y < v ? y : v * 2 - y - 2;
+    shape += Array(v - w).join(' ') + Array(w + 1).join('* ') + '*\n';
+  }
+  console.log(shape)
+  }
+  diamond(5); 
